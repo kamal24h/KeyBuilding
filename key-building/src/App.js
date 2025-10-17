cc94edc6aeb26ae446c46acdfcda393cae1f3de0
kamal24h <kbastani@gmail.com>
2025-10-17 14:51:54 +0330

    ++

--- key-building/src/App.js
+++ key-building/src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Flex, Text, Button } from "@radix-ui/themes";
+import PopoverDemo from './demo/PopoverDemo';
+import AccordionDemo from './Accordion/AccordionDemo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Flex direction="column" gap="2">
+          <Text>Hello from Radix Themes :)</Text>
+          <Button>Let's go</Button>
+        
+          <PopoverDemo />        
+          <AccordionDemo />
+        </Flex>
+      </header>      
     </div>
   );
 }
 
 export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
